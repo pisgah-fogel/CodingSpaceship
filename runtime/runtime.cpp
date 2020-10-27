b9816ab5ca9f83c9efb9b3bf120fe46b2df318cf
Phileas Fogg <phileas.fogg@mail.com>
2020-10-27 16:38:14 +0100

    Parsing command line arguments

--- runtime/runtime.cpp
+++ runtime/runtime.cpp
@@ -1,8 +1,8 @@
-#include<stdio.h> 
-#include<stdlib.h> 
-#include<unistd.h> 
-#include<sys/types.h> 
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <cstring>
 
 #define NUM_PIPES          2
 #define PARENT_WRITE_PIPE  0
@@ -19,7 +19,68 @@ int pipes[NUM_PIPES][2];
 #define CHILD_READ_FD   ( pipes[PARENT_WRITE_PIPE][READ_FD]  )
 #define CHILD_WRITE_FD  ( pipes[PARENT_READ_PIPE][WRITE_FD]  )
 
+struct {
+    char* output;
+    char* binary;
+} Args;
+
+char default_output[] = "sim.xml";
+
+// Parse command line arguments and fill struct Args accordingly
+void parse_args(int argc, char** argv)
+{
+    Args.output = NULL;
+    Args.binary = NULL;
+    for (size_t i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+            if (strcmp(argv[i], "-o") == 0) {
+                if (i+2 > argc) {
+                    puts("\"-o\" must be followed by the output file name (or path)\n");
+                    exit(1);
+                }
+                else if (Args.output != NULL) {
+                    puts("\"-o\" should not be used twice\n");
+                    exit(1);
+                }
+                Args.output = argv[i+1];
+                i++;
+            }
+            else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+                printf("argv[%d] = %s\n", i, argv[i]);
+                puts("Usage: ./runtime <program to test> [-o <.xml output file>]\n");
+                exit(0);
+            }
+            else {
+                printf("Unrecognized option \"%s\"\n", argv[i]);
+                exit(1);
+            }
+        }
+        else {
+            if (Args.binary != NULL) {
+                // TODO: Enable multiple program to play against each others
+                puts("Error: too many input program (command line arguments)\n");
+                exit(1);
+            }
+            Args.binary = argv[i];
+        }
+        
+    }
+
+    if (Args.binary == NULL) {
+        puts("Error: Please provide a program to test\n");
+        exit(1);
+    }
+    if (Args.output == NULL) {
+        printf("[i] Using default output file...\n");
+        Args.output = default_output;
+    }
+    printf("[i] Output file is: %s\n", Args.output);
+}
+
 int main(int argc, char** argv) {
+    
+    parse_args(argc, argv);
+
     int outfd[2];
     int infd[2];
 
@@ -28,7 +89,7 @@ int main(int argc, char** argv) {
     pipe(pipes[PARENT_WRITE_PIPE]);
      
     if(!fork()) {
-        char *argv[]={ "/usr/bin/bc", "-q", 0};
+         char* argv[]={ &(*Args.binary), 0};
  
         dup2(CHILD_READ_FD, STDIN_FILENO);
         dup2(CHILD_WRITE_FD, STDOUT_FILENO);
@@ -39,7 +100,8 @@ int main(int argc, char** argv) {
         close(CHILD_WRITE_FD);
         close(PARENT_READ_FD);
         close(PARENT_WRITE_FD);
-          
+
+        printf("[.] Running: '%s'...\n", Args.binary);
         execv(argv[0], argv);
     } else {
         char buffer[100];
@@ -50,7 +112,19 @@ int main(int argc, char** argv) {
         close(CHILD_WRITE_FD);
  
         // Write to child’s stdin
-        write(PARENT_WRITE_FD, "2^32\n", 5);
+        write(PARENT_WRITE_FD, "2^6\n", 4);
+  
+        // Read from child’s stdout
+        count = read(PARENT_READ_FD, buffer, sizeof(buffer)-1);
+        if (count >= 0) {
+            buffer[count] = 0;
+            printf("%s", buffer);
+        } else {
+            printf("IO Error\n");
+        }
+
+        // Write to child’s stdin
+        write(PARENT_WRITE_FD, "2^3\n", 4);
   
         // Read from child’s stdout
         count = read(PARENT_READ_FD, buffer, sizeof(buffer)-1);
